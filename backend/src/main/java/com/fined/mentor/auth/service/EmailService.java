8effe44c91046a35ab56a0d23eb9fdcf4a29eef1
Loic Maxwell Ngouabe <loic.maxwell.tchiabe.ngouabe@mni.thm.de>
2026-02-06 10:51:54 +0100

    fix:added mailgun for mail sending

--- backend/src/main/java/com/fined/mentor/auth/service/EmailService.java
+++ backend/src/main/java/com/fined/mentor/auth/service/EmailService.java
@@ -3,8 +3,10 @@ package com.fined.mentor.auth.service;
 import com.fined.mentor.auth.exception.EmailException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -13,57 +15,67 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EmailService {
 
-    private final JavaMailSender mailSender;
+    @Value("${mailgun.api.key}")
+    private String apiKey;
+
+    @Value("${mailgun.domain}")
+    private String domain;
+
+    @Value("${mailgun.from.email}")
+    private String fromEmail;
 
     @Async
     public void sendActivationEmail(String toEmail, String username, String activationToken) {
-        try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(toEmail);
-            message.setSubject("Activate Your FinEd Mentor Account");
-            message.setText(
-                    String.format(
-                            "Hello %s,\n\n" +
-                                    "Welcome to FinEd Mentor! Please use the following OTP to activate your account:\n\n"
-                                    +
-                                    "OTP: %s\n\n" +
-                                    "This OTP will expire in 24 hours.\n\n" +
-                                    "If you didn't create an account, please ignore this email.\n\n" +
-                                    "Best regards,\n" +
-                                    "FinEd Mentor Team",
-                            username, activationToken));
+        String subject = "Activate Your FinEd Mentor Account";
+        String content = String.format(
+                "Hello %s,\n\n" +
+                        "Welcome to FinEd Mentor! Please use the following OTP to activate your account:\n\n" +
+                        "OTP: %s\n\n" +
+                        "This OTP will expire in 24 hours.\n\n" +
+                        "If you didn't create an account, please ignore this email.\n\n" +
+                        "Best regards,\n" +
+                        "FinEd Mentor Team",
+                username, activationToken);
 
-            mailSender.send(message);
-            log.info("Activation email sent to: {}", toEmail);
-        } catch (Exception e) {
-            log.error("Failed to send activation email to: {}", toEmail, e);
-            throw new EmailException("Failed to send activation email");
-        }
+        sendEmail(toEmail, subject, content);
     }
 
     @Async
     public void sendPasswordResetEmail(String toEmail, String username, String resetToken) {
-        try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(toEmail);
-            message.setSubject("Reset Your FinEd Mentor Password");
-            message.setText(
-                    String.format(
-                            "Hello %s,\n\n" +
-                                    "We received a request to reset your password. Please use the following OTP to reset it:\n\n"
-                                    +
-                                    "OTP: %s\n\n" +
-                                    "This OTP will expire in 1 hour.\n\n" +
-                                    "If you didn't request a password reset, please ignore this email.\n\n" +
-                                    "Best regards,\n" +
-                                    "FinEd Mentor Team",
-                            username, resetToken));
+        String subject = "Reset Your FinEd Mentor Password";
+        String content = String.format(
+                "Hello %s,\n\n" +
+                        "We received a request to reset your password. Please use the following OTP to reset it:\n\n" +
+                        "OTP: %s\n\n" +
+                        "This OTP will expire in 1 hour.\n\n" +
+                        "If you didn't request a password reset, please ignore this email.\n\n" +
+                        "Best regards,\n" +
+                        "FinEd Mentor Team",
+                username, resetToken);
 
-            mailSender.send(message);
-            log.info("Password reset email sent to: {}", toEmail);
+        sendEmail(toEmail, subject, content);
+    }
+
+    private void sendEmail(String to, String subject, String text) {
+        try {
+            HttpResponse<JsonNode> response = Unirest.post("https://api.mailgun.net/v3/" + domain + "/messages")
+                    .basicAuth("api", apiKey)
+                    .field("from", fromEmail)
+                    .field("to", to)
+                    .field("subject", subject)
+                    .field("text", text)
+                    .asJson();
+
+            if (response.isSuccess()) {
+                log.info("Email successfully sent to: {} via Mailgun. Response: {}", to, response.getBody());
+            } else {
+                log.error("Failed to send email to: {}. Status: {}, Error: {}", to, response.getStatus(),
+                        response.getBody());
+                throw new EmailException("Failed to send email via Mailgun");
+            }
         } catch (Exception e) {
-            log.error("Failed to send password reset email to: {}", toEmail, e);
-            throw new EmailException("Failed to send password reset email");
+            log.error("Exception occurred while sending email to: {}", to, e);
+            throw new EmailException("Exception occurred while sending email via Mailgun");
         }
     }
 }
